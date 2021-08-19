f63ffdcc1d5a80be34525d640000e61e413e00d9
Yusuff Jamal <yusuffjamal3@gmail.com>
2021-08-19 21:08:29 +0200

    Heroku fix

--- client/src/api/AxiosClient.js
+++ client/src/api/AxiosClient.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const AxiosClient = axios.create({
   baseURL: 'https://eren-hng-stage-2.herokuapp.com/api/',
   timeout: 10000,
+  headers: { 'Access-Control-Allow-Origin': '*' },
 });
 
-export default AxiosClient
+export default AxiosClient;
